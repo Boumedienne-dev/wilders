e730a8e3620500fa403688d31ee753172b454c7e
Boumedienne-dev <bmessadia83@gmail.com>
2022-05-09 14:48:51 +0200

    create index.js, json, .gitignore

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+console.log(chalk.blue("Boum Blue"));
+console.log(chalk.red("Laurent Red"));
+console.log(chalk.green("Florence Green"));
+console.log(chalk.yellow("Thomas Yellow"));
+
